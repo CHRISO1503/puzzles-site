d20270070d19bf628752748cb3657c3170e799bc
CHRISO1503 <christopher_oakes@hotmail.com>
2022-12-04 00:22:21 +0000

    Completed base Hue game with editable anchors

--- src/routes/hue.tsx
+++ src/routes/hue.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useEffect, useState, KeyboardEvent, useRef } from "react";
 import "../styles/hue.css";
 export type Nullable<T> = T | null;
+//make selectable anchor points in browser. while selecting, set styling to anchor point on hover
 
 function Hue() {
     const squareWidth = 100;
@@ -11,18 +12,32 @@ function Hue() {
     const [xGradient, setXGradient] = useState([] as number[]);
     const [yGradient, setYGradient] = useState([] as number[]);
     const [originColour, setOriginColour] = useState([] as number[]);
-    const anchorPoints = [
+    const [anchorPoints, setAnchorPoints] = useState([
         [0, 0],
         [numberOfRows - 1, 0],
         [0, numberOfColumns - 1],
         [numberOfRows - 1, numberOfColumns - 1],
-    ];
+        [Math.ceil((numberOfRows - 1) / 2), 0],
+        [Math.floor((numberOfRows - 1) / 2), 0],
+        [Math.ceil((numberOfRows - 1) / 2), numberOfColumns - 1],
+        [Math.floor((numberOfRows - 1) / 2), numberOfColumns - 1],
+        [0, Math.ceil((numberOfColumns - 1) / 2)],
+        [0, Math.floor((numberOfColumns - 1) / 2)],
+        [numberOfRows - 1, Math.ceil((numberOfColumns - 1) / 2)],
+        [numberOfRows - 1, Math.floor((numberOfColumns - 1) / 2)],
+    ]);
+    const [pointSelectedCoords, setPointSelectedCoords] = useState(
+        [] as number[]
+    );
+    const [pointSelected, setPointSelected] = useState(false);
+    const [anchoring, setAnchoring] = useState(true);
 
     useEffect(() => {
         initializeColours();
     }, []);
 
     useEffect(() => {
+        setAnchoring(true);
         initializeGrid();
     }, [yGradient]);
 
@@ -33,14 +48,14 @@ function Hue() {
             Math.random() * 10 + 45,
         ]);
         setXGradient([
-            (Math.max(Math.random(), 0.3) - 0.5) * 30,
-            (Math.max(Math.random(), 0.3) - 0.5) * 5,
-            (Math.max(Math.random(), 0.3) - 0.5) * 5,
+            (Math.random() - 0.5) * 30,
+            (Math.random() - 0.5) * 5,
+            (Math.random() - 0.5) * 5,
         ]);
         setYGradient([
-            (Math.max(Math.random(), 0.3) - 0.5) * 30,
-            (Math.max(Math.random(), 0.3) - 0.5) * 5,
-            (Math.max(Math.random(), 0.3) - 0.5) * 5,
+            (Math.random() - 0.5) * 30,
+            (Math.random() - 0.5) * 5,
+            (Math.random() - 0.5) * 5,
         ]);
     }
 
@@ -63,9 +78,13 @@ function Hue() {
     }
 
     function shuffleBoard() {
+        setAnchoring(false);
         for (let i = 0; i < numberOfRows; i++) {
             for (let j = 0; j < numberOfColumns; j++) {
-                if (anchorPoints.findIndex((c) => c[0] === i && c[1] === j) === -1) {
+                if (
+                    anchorPoints.findIndex((c) => c[0] === i && c[1] === j) ===
+                    -1
+                ) {
                     let swapI = Math.floor(Math.random() * numberOfRows);
                     let swapJ = Math.floor(Math.random() * numberOfColumns);
                     while (
@@ -89,14 +108,64 @@ function Hue() {
         return u.map((x, i) => x + n * v[i]);
     }
 
+    function handleClick(i: number, j: number) {
+        if (anchoring) {
+            if (
+                anchorPoints.findIndex((c) => c[0] === i && c[1] === j) !== -1
+            ) {
+                //remove anchor point
+            } else {
+                anchorPoints.push([i, j]);
+                setAnchorPoints(anchorPoints);
+                setGrid(grid.slice());
+            }
+        } else {
+            if (
+                anchorPoints.findIndex((c) => c[0] === i && c[1] === j) !== -1
+            ) {
+                return;
+            }
+            if (pointSelected) {
+                let temp = grid[i][j];
+                grid[i][j] =
+                    grid[pointSelectedCoords[0]][pointSelectedCoords[1]];
+                grid[pointSelectedCoords[0]][pointSelectedCoords[1]] = temp;
+                setGrid(grid.slice());
+                setPointSelected(false);
+            } else {
+                setPointSelectedCoords([i, j]);
+                setPointSelected(true);
+            }
+        }
+    }
+
+    function resetAnchorPoints() {
+        setAnchorPoints([]);
+    }
+
     return (
         <>
+            <h1 id="title">Hue</h1>
             <div>
                 {grid.map((row, i) => (
                     <div key={i} style={{ display: "table" }}>
                         {row.map((cell, j) => (
                             <div
                                 key={j}
+                                onClick={() => handleClick(i, j)}
+                                className={`${
+                                    pointSelectedCoords[0] == i &&
+                                    pointSelectedCoords[1] == j &&
+                                    pointSelected
+                                        ? "selected-tile"
+                                        : "tile"
+                                } ${
+                                    anchorPoints.findIndex(
+                                        (c) => c[0] === i && c[1] === j
+                                    ) !== -1
+                                        ? "anchor-point"
+                                        : ""
+                                }`}
                                 style={{
                                     display: "table-cell",
                                     backgroundColor: `hsl(${cell[0]} ${cell[1]}% ${cell[2]}%)`,
@@ -108,9 +177,17 @@ function Hue() {
                     </div>
                 ))}
             </div>
-            <button className="action-button" onClick={shuffleBoard}>
-                Shuffle
-            </button>
+            <div>
+                <button className="action-button" onClick={shuffleBoard}>
+                    Shuffle
+                </button>
+                <button className="action-button" onClick={initializeColours}>
+                    New Game
+                </button>
+                <button className="action-button" onClick={resetAnchorPoints}>
+                    Reset Fixed
+                </button>
+            </div>
         </>
     );
 }
